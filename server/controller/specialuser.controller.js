918dd852e6c2a5f0541243e2649539a7ef25872b
RMDcode <dhurir163@gmail.com>
2024-06-13 16:36:59 +0530

    contact form1

--- server/controller/specialuser.controller.js
+++ server/controller/specialuser.controller.js
@@ -1,26 +1,20 @@
-import Special from "../modal/specialuser.modal.js";
 import nodemailer from 'nodemailer';
+import Special from "../modal/specialuser.modal.js";
 
 export const special = async (req, res) => {
     const { firstname, lastname, email, phone, education, address, batch } = req.body;
 
-    const se_students = new Special({
-        firstname,
-        lastname,
-        email,
-        phone,
-        education,
-        address,
-        batch,
-    });
+        const se_students = new Special({
+            firstname,
+            lastname,
+            email,
+            phone,
+            education,
+            address,
+            batch
+        });
 
-    try {
-        // Check if the email already exists in the collection
-        const existingStudent = await se_students.findOne({ email });
-        if (existingStudent) {
-            return res.status(400).json({ success: false, message: 'Email already exists. Please use a different email address.' });
-        }
-
+    try{
         await se_students.save();
 
         // Set up Nodemailer transporter
@@ -83,7 +77,7 @@ email: dhurir163@gmail.com
         return res.json({ success: true, message: 'Your details have been submitted and email has been sent.' });
 
     } catch (err) {
-        res.json({ success: false, message: "Error saving details" });
-        console.error(err);
+        console.error('Error saving details:', err);
+        return res.status(500).json({ success: false, message: 'Error occurred while saving details.' });
     }
 };
